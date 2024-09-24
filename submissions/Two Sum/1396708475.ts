7d67fc5892a69dd69e8c8f5286b8f6c45238258e
Gurucharan <gurubgshetty@gmail.com>
2024-09-24 11:10:43 +0530

    Add submission 1396708475

---
+++ submissions/Two Sum/1396708475.ts
@@ -0,0 +1,18 @@
+# Title: Two Sum
+# Submission ID: 1396708475
+# Status: Accepted
+# Date: September 20, 2024 at 11:24:01 PM GMT+5:30
+
+function twoSum(nums: number[], target: number): number[] {
+    const mem: { [key: number]: number } = {};  // Object to map number to its index
+    const n = nums.length;
+
+    for (let i = 0; i < n; i++) {
+        const x = target - nums[i];
+        if (x in mem) {
+            return [mem[x], i];
+        }
+        mem[nums[i]] = i;
+    }
+    return [];
+}
